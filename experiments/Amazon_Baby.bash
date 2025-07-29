a36e987250e3e6c6f1bd8c5e76b746a2b5cf6fe7
0-1user <63350404+0-1user@users.noreply.github.com>
2025-07-29 21:15:39 +0800

    Update Amazon_Baby.bash

--- experiments/Amazon_Baby.bash
+++ experiments/Amazon_Baby.bash
@@ -1,12 +1,12 @@
 ## baseline "SIGMA" "EchoMamba4Rec" "Mamba4Rec" "LinRec" "CFIT4SRec" "FEARec" "FMLPRec" "SASRec" "GRU4Rec" "BERT4Rec"
 ##datasets: Amazon_Sports Amazon_Baby Amazon_Video_Games Amazon_Office_Products
-gpu_id=0    #gpu id 更改
+gpu_id=0   
 dataset="Amazon_Baby"
 models=("FAESRec") # model
 
 for model in ${models[@]}
 do
-    echo "当前模型：$model"
+    echo "model：$model"
     python run_seq.py --dataset ${dataset} \   --model ${model} \ --gpu_id ${gpu_id} \
     
 done
